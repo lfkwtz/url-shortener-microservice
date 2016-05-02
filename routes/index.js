f805bb5c6c0983f33e00bbcb7d0b824c778bd2f1
leftynaut <mlefkowitz87@gmail.com>
2016-05-02 14:13:25 -0700

    http:// now works. still need to validate.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
-var assert = require('assert');
 
 var shortid = require('shortid');
 
@@ -10,41 +9,7 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-// router.get('/newurl', function(req,res){
-//   res.render('newurl', {title: 'Add URL'});
-// });
-
-// router.post('/newurl', function(req,res){
-//   var MongoClient = mongodb.MongoClient
-
-//   var url = 'mongodb://localhost:27017/url2'
-
-//   MongoClient.connect(url, function(err, db){
-//     if (err){
-//       console.log("Unable to connect to server", err);
-//     } else {
-//       console.log("Connected to server")
-
-//       var collection = db.collection('urls');
-
-//       var findUrl = {short: req.body.url};
-//       var print = collection.find({"short": findUrl});
-//       console.log(print);
-
-//       var newUrl = {url: req.body.url, short: shortid.generate()};
-//       collection.insert([newUrl], function(err, result){
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           res.render('index');
-//         }
-//         db.close();
-//       });
-//     }
-//   });
-// });
-
-router.get('/:entry', function(req, res, next) {
+router.get('/:entry(*)', function(req, res, next) {
   var MongoClient = mongodb.MongoClient
 
   var url = 'mongodb://localhost:27017/url2'
@@ -58,7 +23,7 @@ router.get('/:entry', function(req, res, next) {
     var collection = db.collection('urls');
     var entry = req.params.entry;
     
-    var local = "localhost:3000/";
+    var local = req.get('host');
     
     var findLink = function(db, callback) {
     collection.findOne( { "short": entry }, {url: 1, _id: 0}, function(err, doc) {
@@ -70,7 +35,7 @@ router.get('/:entry', function(req, res, next) {
           var short = shortid.generate();
           var newUrl = {url: entry, short: short};
           collection.insert([newUrl]);
-          res.json({original_url: entry, short_url: local+short});
+          res.json({original_url: entry, short_url: local+'/'+short});
         };
       });
     };
