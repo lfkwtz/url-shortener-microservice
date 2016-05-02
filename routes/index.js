db06804d77629bd6e3702abb63198b387a906973
leftynaut <mlefkowitz87@gmail.com>
2016-05-02 13:12:45 -0700

    working. but needs url validation.

--- routes/index.js
+++ routes/index.js
@@ -10,39 +10,39 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/newurl', function(req,res){
-  res.render('newurl', {title: 'Add URL'});
-});
+// router.get('/newurl', function(req,res){
+//   res.render('newurl', {title: 'Add URL'});
+// });
 
-router.post('/newurl', function(req,res){
-  var MongoClient = mongodb.MongoClient
+// router.post('/newurl', function(req,res){
+//   var MongoClient = mongodb.MongoClient
 
-  var url = 'mongodb://localhost:27017/url2'
+//   var url = 'mongodb://localhost:27017/url2'
 
-  MongoClient.connect(url, function(err, db){
-    if (err){
-      console.log("Unable to connect to server", err);
-    } else {
-      console.log("Connected to server")
+//   MongoClient.connect(url, function(err, db){
+//     if (err){
+//       console.log("Unable to connect to server", err);
+//     } else {
+//       console.log("Connected to server")
 
-      var collection = db.collection('urls');
+//       var collection = db.collection('urls');
 
-      var findUrl = {short: req.body.url};
-      var print = collection.find({"short": findUrl});
-      console.log(print);
+//       var findUrl = {short: req.body.url};
+//       var print = collection.find({"short": findUrl});
+//       console.log(print);
 
-      var newUrl = {url: req.body.url, short: shortid.generate()};
-      collection.insert([newUrl], function(err, result){
-        if (err) {
-          console.log(err);
-        } else {
-          res.render('index');
-        }
-        db.close();
-      });
-    }
-  });
-});
+//       var newUrl = {url: req.body.url, short: shortid.generate()};
+//       collection.insert([newUrl], function(err, result){
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           res.render('index');
+//         }
+//         db.close();
+//       });
+//     }
+//   });
+// });
 
 router.get('/:entry', function(req, res, next) {
   var MongoClient = mongodb.MongoClient
@@ -58,13 +58,19 @@ router.get('/:entry', function(req, res, next) {
     var collection = db.collection('urls');
     var entry = req.params.entry;
     
+    var local = "localhost:3000/";
+    
     var findLink = function(db, callback) {
     collection.findOne( { "short": entry }, {url: 1, _id: 0}, function(err, doc) {
     if (doc != null) {
-      console.log('URL Found!!');
-      res.redirect("http://"+doc.url);
+      console.log('URL Found!');
+      res.redirect(doc.url);
         } else {
           console.log('URL NOT FOUND! NEW ENTRY!');
+          var short = shortid.generate();
+          var newUrl = {url: entry, short: short};
+          collection.insert([newUrl]);
+          res.json({original_url: entry, short_url: local+short});
         };
       });
     };
