665f715ececec786a0a60745bf9970ae0c1289cd
leftynaut <mlefkowitz87@gmail.com>
2016-05-03 10:31:12 -0700

    indent party

--- routes/index.js
+++ routes/index.js
@@ -11,43 +11,43 @@ shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX
 var validUrl = require('valid-url');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.get('/new/:url(*)', function(req, res, next) {
+router.get('/new/:url(*)', function (req, res, next) {
   var MongoClient = mongodb.MongoClient
-  
-  MongoClient.connect(mLab, function(err, db){
-    if (err){
+
+  MongoClient.connect(mLab, function (err, db) {
+    if (err) {
       console.log("Unable to connect to server", err);
     } else {
       console.log("Connected to server")
-      
+
       var collection = db.collection('urls');
       var url = req.params.url;
-      
+
       //sets current hostname to var local
       var local = req.get('host');
-      
-      var newLink = function(db, callback) {
-            collection.findOne( { "url": url }, {short: 1, _id: 0}, function(err, doc) {
-            if (doc != null) {
-              res.json({original_url: url, short_url: local+'/'+doc.short});
-            } else {
-            if (validUrl.isUri(url)){
+
+      var newLink = function (db, callback) {
+        collection.findOne({ "url": url }, { short: 1, _id: 0 }, function (err, doc) {
+          if (doc != null) {
+            res.json({ original_url: url, short_url: local + '/' + doc.short });
+          } else {
+            if (validUrl.isUri(url)) {
               var short = shortid.generate();
-              var newUrl = {url: url, short: short};
+              var newUrl = { url: url, short: short };
               collection.insert([newUrl]);
-              res.json({original_url: url, short_url: local+'/'+short});
+              res.json({ original_url: url, short_url: local + '/' + short });
             } else {
-              res.json({error: "Wrong url format, make sure you have a valid protocol and real site."});
+              res.json({ error: "Wrong url format, make sure you have a valid protocol and real site." });
             };
-            };
-            });
-        };
-      
-      newLink(db, function() {
+          };
+        });
+      };
+
+      newLink(db, function () {
         db.close();
       });
 
@@ -55,29 +55,29 @@ router.get('/new/:url(*)', function(req, res, next) {
   });
 });
 
-router.get('/:short', function(req, res, next) {
+router.get('/:short', function (req, res, next) {
   var MongoClient = mongodb.MongoClient
 
-  MongoClient.connect(mLab, function(err, db){
-    if (err){
+  MongoClient.connect(mLab, function (err, db) {
+    if (err) {
       console.log("Unable to connect to server", err);
     } else {
       console.log("Connected to server")
-      
+
       var collection = db.collection('urls');
       var short = req.params.short;
-            
-      var findLink = function(db, callback) {
-      collection.findOne( { "short": short }, {url: 1, _id: 0}, function(err, doc) {
-      if (doc != null) {
-        res.redirect(doc.url);
+
+      var findLink = function (db, callback) {
+        collection.findOne({ "short": short }, { url: 1, _id: 0 }, function (err, doc) {
+          if (doc != null) {
+            res.redirect(doc.url);
           } else {
-            res.json({error: "No corresponding shortlink found in the database."});
+            res.json({ error: "No corresponding shortlink found in the database." });
           };
         });
       };
-      
-      findLink(db, function() {
+
+      findLink(db, function () {
         db.close();
       });
 
