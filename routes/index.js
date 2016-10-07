251b783376d69f5a3ca3d3fbf328e0aa3e3e68bb
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-07 18:17:17 -0500

    mongoose rewrite working

--- routes/index.js
+++ routes/index.js
@@ -12,51 +12,53 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/new/:url(*)', (req, res, next) => {
-  let url = req.params.url;
-  // check if exists, if it does - return document
-  Link.findOne({ url }).then(found => {
-    if (found) {
-      res.status(200).send(found);
-    } else {
-      console.log('not found')
-      helper.urlCheck(url, function(valid) {
-        if (valid) {
-          let createLink = new Link({ url });
-          createLink.save()
-          .then(newDoc => {
-            // res.json({
-            //     original_url: params,
-            //     short_url: `${req.get('host')}/${shortCode}`
-            // });
-            res.status(200).send(newDoc);
-          });
+    let url = req.params.url;
+    // check if exists, if it does - return document
+    Link.findOne({
+        url
+    }).then(found => {
+        if (found) {
+          let obj = {
+                original_url: found.url,
+                short_url: `${req.get('host')}/${found.short}`
+                }
+            res.status(200).json(obj);
         } else {
-          res.json({error: 'Wrong url format, make sure you have a valid protocol and real site.'});
+            console.log('not found')
+            helper.urlCheck(url, function(valid) {
+                if (valid) {
+                    let createLink = new Link({
+                        url
+                    });
+                    createLink.save()
+                        .then(newDoc => {
+                          let obj = {
+                                original_url: newDoc.url,
+                                short_url: `${req.get('host')}/${newDoc.short}`
+                                }
+                            res.status(200).json(obj);
+                        });
+                } else {
+                    res.json({
+                        error: 'Wrong url format, make sure you have a valid protocol and real site.'
+                    });
+                }
+            })
         }
-      })
-    }
-  })
+    })
 });
 
 router.get('/:short', (req, res, next) => {
-  let shorty = req.params.short;
-  Link.findOne({})
-    let findLink = function(db, callback) {
-        collection.findOne({
-            'short': params
-        }, {
-            url: 1,
-            _id: 0
-        }, (err, doc) => {
-            doc ?
-                res.redirect(doc.url) :
-                res.json({
-                    error: 'No corresponding shortlink found in the database.'
-                })
-        });
-            };
-        };
-    });
+    let shorty = req.params.short;
+    Link.findOne({}).then(link => {
+        if (link) {
+            res.status(200).send(link);
+        } else {
+            res.json({
+                error: 'No corresponding shortlink found in the database.'
+            })
+        }
+    })
 });
 
 module.exports = router;
